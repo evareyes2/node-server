bf05eb27744ece729dc76085ec62c880fdc60abd
Eva Reyes <macbook@MacBook-Air-de-MacBook.local>
2023-09-20 11:29:36 -0500

    cambios node

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# node-server
+# Lista de tareas
+*** 
+
+
+## ¿Qué sucedió al usar async y await?
+Al modificar las funciones para que retornen una promesa y usar 'async' y 'await' hace que el flujo de control sea más legible y facil de entender, ya que cada promesa se resulve antes de continuar con la sigueinte accion, ejecuntandose de manera secuencia. 'async' y 'await' también nos permite que las opreaciones se ralicen en el orden que pongamos en el codigo. 
+
+## ¿Qué sucedió al usar el método then()?
+Al usar el método '.then' nos aseguramos de manejar el resultado exito de una promesa. Y así podemos definir acciones personalizadas que se ejecutarán cuando las operaciones asincrónicas tengan éxito. aunque con este metodo el cosdigo podria volverse mas complicado, por que tendriamos que encadenar mas acciones. 
+
+## ¿Qué diferencias encontraste entre async, await y el método then()?
+El método then() se utiliza para manejar el resultado exitoso de una promesa. 
+'async' y 'await' nos facilita la comprension y legibilidad del codigo y cuando se trata de errores es mas facil manejarlos con 'try' y 'catch'
--- app.js
+++ app.js
@@ -1,53 +1,116 @@
 class Task {
-    constructor(description) {
-      this.description = description;
-      this.completed = false;
+  constructor(description) {
+    this.description = description;
+    this.completed = false;
+  }
+}
+
+class TaskList {
+  constructor() {
+    this.tasks = [];
+  }
+
+  addTask(description) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (description) {
+          const task = new Task(description);
+          this.tasks.push(task);
+          resolve(task);
+        } else {
+          reject(new Error('La descripción de la tarea no puede estar vacía.'));
+        }
+      }, 2000);
+    });
+  }
+
+  completeTask(index) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (index >= 0 && index < this.tasks.length) {
+          this.tasks[index].completed = true;
+          resolve();
+        } else {
+          reject(new Error('Índice de tarea inválido.'));
+        }
+      }, 1000);
+    });
+  }
+
+  deleteTask(index) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (index >= 0 && index < this.tasks.length) {
+          this.tasks.splice(index, 1);
+          resolve();
+        } else {
+          reject(new Error('Índice de tarea inválido.'));
+        }
+      }, 4000);
+    });
+  }
+
+  async runTaskList() {
+    try {
+      await this.addTask('Estudiar');
+      await this.addTask('Hacer mercado');
+      await this.addTask('Leer un libro');
+
+      this.listTasks();
+
+      await this.completeTask(0);
+      this.listTasks();
+
+      await this.deleteTask(1);
+      this.listTasks();
+    } catch (error) {
+      console.error(error.message);
     }
   }
-  
-  class TaskList {
-    constructor() {
-      this.tasks = [];
-    }
-  
-    addTask(description) {
-      const task = new Task(description);
-      this.tasks.push(task);
-    }
-  
-    completeTask(index) {
-      if (index >= 0 && index < this.tasks.length) {
-        this.tasks[index].completed = true;
-      }
-    }
-  
-    deleteTask(index) {
-      if (index >= 0 && index < this.tasks.length) {
-        this.tasks.splice(index, 1);
-      }
-    }
-  
-    listTasks() {
-      console.log('\nLista de Tareas:');
-      this.tasks.forEach((task, index) => {
-        console.log(`${index + 1}. [${task.completed ? 'X' : ' '}] ${task.description}`);
-      });
-      console.log();
-    }
+
+  listTasks() {
+    console.log('\nLista de Tareas:');
+    this.tasks.forEach((task, index) => {
+      console.log(`${index + 1}. [${task.completed ? 'X' : ' '}] ${task.description}`);
+    });
+    console.log();
   }
-  
-  const taskList = new TaskList();
-  
-  taskList.addTask('Estudiar');
-  taskList.addTask('Hacer mercado');
-  taskList.addTask('Leer un libro');
+}
+
+const taskList = new TaskList();
+
+taskList.runTaskList();
+
+
+//   listTasks() {
+//     console.log("\nLista de Tareas:");
+//     this.tasks.forEach((task, index) => {
+//       console.log(`${index + 1}. [${task.completed ? 'X' : ' '}] ${task.description}`);
+//     });
+//     console.log();
+//   }
+// }
+
+
+
+// const taskList = new TaskList();
+
+// taskList.addTask("Estudiar")
+// .then(() => taskList.addTask('Hacer mercado'))
+// .then(() => taskList.addTask('Leer un libro'))
+// .then(() => taskList.listTasks())
+// .then(() => taskList.completeTask(0))
+// .then(() => taskList.listTasks())
+// .then(() => taskList.deleteTask(1))
+// .then(() => taskList.listTasks())
+// .catch((error) => console.log(error.message));
+// // taskList.addTask("Leer un libro");
 //   taskList.addTask('');
-  
-  taskList.listTasks();
-  
-  taskList.completeTask(0);
-  taskList.listTasks();
-  
-  taskList.deleteTask(1);
-  taskList.listTasks();
-  
+
+// taskList.listTasks();
+
+// taskList.completeTask(0);
+// taskList.listTasks();
+
+// taskList.deleteTask(1);
+// taskList.listTasks();
